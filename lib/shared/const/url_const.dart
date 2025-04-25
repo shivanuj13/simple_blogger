98e61c03e6d33186ec4e7377856b5489dbc55d60
Anuj <anuj.kumar@likeminds.community>
2025-04-26 00:57:11 +0530

    fix: fixed flutter version and backend url

--- lib/shared/const/url_const.dart
+++ lib/shared/const/url_const.dart
@@ -1,3 +1,3 @@
 const String devUrl = "http://10.0.2.2:3000";
-const String prodUrl = "https://simpleblog-1-a8789157.deta.app";
+const String prodUrl = "https://simple-blogger-backend.onrender.com";
 const String baseUrl = prodUrl;
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,15 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
